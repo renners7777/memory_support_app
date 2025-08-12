1bf81d6449f3866c62b350254df123e94291b401
Chris Renshaw <ChrisRenshaw7@outlook.com>
2025-08-13 00:00:19 +0100

    final clean of build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,29 +7,27 @@ plugins {
 android {
     namespace = "com.chrisrenshaw.memory_support_app"
     compileSdk = 35
-    ndkVersion = "27.0.12077973"
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
 
     defaultConfig {
         applicationId = "com.chrisrenshaw.memory_support_app"
         minSdk = 21
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     buildTypes {
+        debug {
+            signingConfig = signingConfigs.getByName("debug")
+        }
         release {
             signingConfig = signingConfigs.getByName("debug")
         }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 }
 
