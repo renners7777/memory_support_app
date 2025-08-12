a95223723840295183e7b699a3fcaefc4b211c6e
Chris Renshaw <ChrisRenshaw7@outlook.com>
2025-08-12 23:47:46 +0100

    Updated build.grade.kts file

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -20,11 +19,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.chrisrenshaw.memory_support_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        // The `local_auth` plugin requires a minimum SDK of 21.
         minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -34,15 +29,8 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("debug")
-            // Add output directory configuration
-            applicationVariants.all {
-                outputs.all {
-                    outputFile = file("$buildDir/outputs/apk/${name}/app-${name}.apk")
-                }
-            }
         }
     }
-
 }
 
 flutter {
